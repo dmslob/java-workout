f95f16df4472fcad21bd96e2c6852ec93bda5ab1
Dmytro Slobodeniuk <dmytro.slobodeniuk@dxc.com>
2025-02-26 15:34:07 +0200

    PalindromeTest is updated

--- adt/src/test/java/com/dmslob/problems/PalindromeTest.java
+++ adt/src/test/java/com/dmslob/problems/PalindromeTest.java
@@ -1,13 +1,72 @@
 package com.dmslob.problems;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class PalindromeTest {
 
-    private final Palindrome palindrome = new Palindrome();
+    boolean valid(String word) {
+        String reversedWord = new StringBuilder(word).reverse().toString();
+        return word.equalsIgnoreCase(reversedWord);
+    }
+
+    /**
+     * Takes a number and checks whether it is a palindrome
+     *
+     * @return true if palindrome
+     */
+    boolean valid(int number) {
+        int k = 0;
+        int argNumber = number;
+        while (number > 0) {
+            k = k * 10 + (number % 10);
+            number = number / 10;
+        }
+        return k == argNumber;
+    }
+
+    boolean isPermutationOfPalindrome(String s) {
+        var table = new int[128];
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i)]++;
+            if (table[s.charAt(i)] % 2 == 0) {
+                count--;
+            } else {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+
+    boolean canPermutePalindrome1(String s) {
+        var set = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!set.add(s.charAt(i))) {
+                set.remove(s.charAt(i));
+            }
+        }
+        return set.size() <= 1;
+    }
+
+    boolean canPermutePalindrome2(String s) {
+        var map = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            var ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        int count = 0;
+        for (char key : map.keySet()) {
+            count += map.get(key) % 2;
+        }
+        return count <= 1;
+    }
 
     @DataProvider
     public static Object[][] words_to_check() {
@@ -22,7 +81,7 @@ public class PalindromeTest {
     public static Object[][] numbers_to_check() {
         return new Object[][]{
                 {1221, true}, {1001, true}, {100001, true}, {541145, true},
-                {122, false}, {111, false}, {1, false}, {0, false}
+                {122, false}, {111, true}, {1, true}, {0, true}
         };
     }
 
@@ -30,7 +89,7 @@ public class PalindromeTest {
     public void should_validate_if_word_is_palindrome(String word, boolean expectedResult) {
         // given
         // when
-        boolean actualResult = palindrome.valid(word);
+        boolean actualResult = valid(word);
         // then
         assertThat(actualResult)
                 .isEqualTo(expectedResult);
@@ -40,7 +99,7 @@ public class PalindromeTest {
     public void should_validate_if_number_is_palindrome(Integer number, boolean expectedResult) {
         // given
         // when
-        boolean actualResult = palindrome.valid(number);
+        boolean actualResult = valid(number);
         // then
         assertThat(actualResult)
                 .isEqualTo(expectedResult);
