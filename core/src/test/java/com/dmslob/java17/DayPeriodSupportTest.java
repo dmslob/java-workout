2f296bd975489f3f52e59b86b7fb6c823f2447bb
dmslob <dmytro.slobodenyuk@gmail.com>
2025-04-13 18:48:42 +0300

    DayPeriodSupportTest is updated

--- core/src/test/java/com/dmslob/java17/DayPeriodSupportTest.java
+++ core/src/test/java/com/dmslob/java17/DayPeriodSupportTest.java
@@ -2,40 +2,33 @@ package com.dmslob.java17;
 
 import org.testng.annotations.Test;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class DayPeriodSupportTest {
 
     @Test
-    public void should_format_with_local_language() {
-        // given
-        var dtf = DateTimeFormatter.ofPattern("B");
-        // when | then
-        System.out.println(dtf.format(LocalTime.of(8, 0)));
-        System.out.println(dtf.format(LocalTime.of(13, 0)));
-        System.out.println(dtf.format(LocalTime.of(20, 0)));
-        System.out.println(dtf.format(LocalTime.of(23, 0)));
-        System.out.println(dtf.format(LocalTime.of(0, 0)));
-
-        System.out.println(DateTimeFormatter.ofPattern("G").format(LocalDate.now()));
-        System.out.println(DateTimeFormatter.ofPattern("z").format(ZonedDateTime.now()));
-        System.out.println(DateTimeFormatter.ofPattern("v").format(ZonedDateTime.now()));
-    }
-
-    @Test
-    public void should_format_with_dutch_language() {
+    public void should_format_with_en_language() {
         // given
         var dtf = DateTimeFormatter.ofPattern("B")
                 .withLocale(Locale.forLanguageTag("en"));
         // when | then
-        System.out.println(dtf.format(LocalTime.of(8, 0)));
-        System.out.println(dtf.format(LocalTime.of(13, 0)));
-        System.out.println(dtf.format(LocalTime.of(20, 0)));
-        System.out.println(dtf.format(LocalTime.of(0, 0)));
-        System.out.println(dtf.format(LocalTime.of(1, 0)));
+        var inTheMorning = dtf.format(LocalTime.of(8, 0));
+        assertThat(inTheMorning).isEqualTo("in the morning");
+
+        var inTheAfternoon = dtf.format(LocalTime.of(13, 0));
+        assertThat(inTheAfternoon).isEqualTo("in the afternoon");
+
+        var inTheEvening = dtf.format(LocalTime.of(20, 0));
+        assertThat(inTheEvening).isEqualTo("in the evening");
+
+        var midnight = dtf.format(LocalTime.of(0, 0));
+        assertThat(midnight).isEqualTo("midnight");
+
+        var atNight = dtf.format(LocalTime.of(1, 0));
+        assertThat(atNight).isEqualTo("at night");
     }
 }
