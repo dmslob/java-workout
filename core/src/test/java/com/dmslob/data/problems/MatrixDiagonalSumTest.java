b545cfb84385cfd0e7ea3dbd33ec123d4192a30d
dmslob <dmytro.slobodenyuk@gmail.com>
2025-04-26 22:08:37 +0300

    MatrixDiagonalSumTest and MoveZerosTest are added

---
+++ core/src/test/java/com/dmslob/data/problems/MatrixDiagonalSumTest.java
@@ -0,0 +1,66 @@
+package com.dmslob.data.problems;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class MatrixDiagonalSumTest {
+
+    /**
+     * Given a square matrix mat, return the sum of the matrix diagonals.
+     * Only include the sum of all the elements on the primary diagonal
+     * and all the elements on the secondary diagonal that are not part of the primary diagonal.
+     * leetcode problem: @see <a href="https://leetcode.com/problems/matrix-diagonal-sum/description/">matrix-diagonal-sum</a>
+     */
+    int diagonalSum(int[][] mat) {
+        int n = mat.length; // since it's a square matrix
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            // Add primary diagonal element
+            sum += mat[i][i];
+            // Add secondary diagonal element
+            sum += mat[i][n - 1 - i];
+        }
+        // If the matrix has an odd size, the center element was counted twice
+        if (n % 2 == 1) {
+            sum -= mat[n / 2][n / 2];
+        }
+        return sum;
+    }
+
+    @Test
+    public void should_calculate_diagonal_sum() {
+        // given
+        int[][] mat = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+        int expectedSum = 25;
+        // when
+        int sum = diagonalSum(mat);
+        // then
+        // Output (1+5+9 + 3+5+7 - 5) = 25
+        assertThat(sum).isEqualTo(expectedSum);
+
+        // given
+        mat = new int[][]{
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1},
+                {1, 1, 1, 1}};
+        expectedSum = 8;
+        // when
+        sum = diagonalSum(mat);
+        // then
+        // Output (1+1+1+1 + 1+1+1+1) = 8
+        assertThat(sum).isEqualTo(expectedSum);
+
+        // given
+        mat = new int[][]{{5}};
+        expectedSum = 5;
+        // when
+        sum = diagonalSum(mat);
+        // then
+        assertThat(sum).isEqualTo(expectedSum);
+    }
+}
---
+++ core/src/test/java/com/dmslob/data/problems/MoveZerosTest.java
@@ -0,0 +1,47 @@
+package com.dmslob.data.problems;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class MoveZerosTest {
+
+    /**
+     * Given an integer array nums, move all 0's to the end of it
+     * while maintaining the relative order of the non-zero elements.
+     * Note that you must do this in-place without making a copy of the array.
+     * leetcode problem: @see <a href="https://leetcode.com/problems/move-zeroes/">move-zeroes</a>
+     */
+    void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        // First pass: move all non-zero elements forward
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+        // Second pass: fill the rest with zeros
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+
+    @Test
+    public void should_move_zeros_to_the_end() {
+        // given
+        int[] nums = {0, 1, 0, 3, 12};
+        int[] expected = {1, 3, 12, 0, 0};
+        // when
+        moveZeroes(nums);
+        // then
+        assertThat(nums).isEqualTo(expected);
+
+        // given
+        nums = new int[]{0};
+        expected = new int[]{0};
+        // when
+        moveZeroes(nums);
+        // then
+        assertThat(nums).isEqualTo(expected);
+    }
+}
